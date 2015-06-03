74e2761d1a6af065f0f6b5504d6aef0df902ccbc
ThaPlayaSlaya <thaplayaslaya@gmail.com>
2015-06-03 14:20:55 -0700

    Cleaned up. Pushing for update.

--- src/com/thaplayaslaya/Main.java
+++ src/com/thaplayaslaya/Main.java
@@ -1,12 +1,28 @@
 package com.thaplayaslaya;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
 	static DeckManager DM;
 
 	public static void main(String[] args) {
+
+		try {
+			// Set cross-platform Java L&F (also called "Metal")
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
